70c8db8430ac1e140c1b88c8e1be48b7f9ba5306
Denis Kudia <dkudja@gmail.com>
2025-04-03 16:10:10 -0700

    Removed not user policy

--- LunarLander.py
+++ LunarLander.py
@@ -22,21 +22,16 @@ but the system also supports multiple data structure representations such as FIF
 import reverb
 
 from tf_agents.agents.dqn import dqn_agent
-from tf_agents.agents.reinforce import reinforce_agent
 from tf_agents.specs import tensor_spec
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 from tf_agents.networks import sequential
 from tf_agents.utils import common
-from tf_agents.utils import composite
 from tf_agents.policies import random_tf_policy
-from tf_agents.policies import random_py_policy
 from tf_agents.policies import py_tf_eager_policy
 from tf_agents.specs import tensor_spec
 from tf_agents.replay_buffers import reverb_replay_buffer
 from tf_agents.replay_buffers import reverb_utils
-from tf_agents.trajectories import trajectory
-from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.drivers import py_driver
 from tf_agents.metrics import tf_metrics
 from tf_agents.networks.layer_utils import print_summary
