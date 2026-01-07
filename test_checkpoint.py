fc4c13a4b712b74fd2f386d1e24f07d2c25f5303
Denis Kudia <dkudja@gmail.com>
2026-01-06 16:54:56 -0800

    Test Load checkpoint.

---
+++ test_checkpoint.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import numpy as np
+import reverb
+
+import tensorflow as tf
+
+
+def load_checkpoint(folder : str) -> any:
+    """Try to load checkpoint from folder"""
+    try:
+        reader = tf.train.load_checkpoint(folder)
+        shape_from_key = reader.get_variable_to_shape_map()
+        dtype_from_key = reader.get_variable_to_dtype_map()
+
+        sorted(shape_from_key.keys())
+        print(shape_from_key.keys())
+
+    except ValueError as verr:
+        print("Could not load checkpoints from {1} Error: {2}".format(folder, verr))
+        return None
+    return folder
+
+
+if __name__ == '__main__':
+    folder = None
+
+    if len(sys.argv) > 1:
+        folder = sys.argv[1]
+    else:
+        print("No checkpoint folder")
+        exit(1)
+
+    res = load_checkpoint(folder)
